5df914fcb0d11d649f88cd6b1f0d21bedd99d54b
Brandon Batten <ba_batten@live.com>
2017-10-04 21:35:39 -0400

    commit previously completed project

---
+++ js/helper.js
@@ -0,0 +1,11 @@
+var HTMLlineItem = '<li>'+
+                '<div class="work-link-pic">'+
+                    '<img src="%image%" alt="%alt%" class="img-responsive" data-toggle="modal" data-target="#%dt%">'+
+                '</div>'+
+              '<a href="%url%" class="navy">'+
+                '<div class="work-link-desc">'+
+                  '<h3 class="text-uppercase">%h3%</h3>'+
+                  '<p>%url%</p>'+
+                '</div>'+
+              '</a>'+
+            '</li>';
